1b1c65db4df1c42763dcf91197f194d56460712f
Sophie Quinn <s.quinn7152002@gmail.com>
2026-03-16 22:34:46 +0000

    Created the interface files.

---
+++ SeagullDockApp/src/seagulldockapp/MessagesInterface.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package seagulldockapp;
+
+/**
+ *
+ * @author Sophie Quinn
+ */
+//This is the queue interface
+public interface MessagesInterface {
+    
+}
